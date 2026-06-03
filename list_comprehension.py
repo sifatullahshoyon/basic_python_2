7d2920b992c4b7b1c50e1050f9a50a51cf83831f
Sifat Ullah Shoyon <sifatullah.shoyon@gmail.com>
2026-06-03 20:21:03 +0600

    list_comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,27 @@
+numbers = [45, 87, 96, 65, 43, 90, 85, 14, 26, 61, 70]
+odds = []
+for num in numbers:
+    if num % 2 == 1 and num % 5 == 0:
+        odds.append(num)
+
+# print(odds)
+
+# odd_nums = [num for num in numbers if num % 2 == 1]
+odd_nums = [num for num in numbers if num % 2 == 1 if num % 5 == 0]
+print(odd_nums)
+
+players = ['sakib', 'tamim', 'liton']
+ages = [38, 37, 35]
+
+age_comb = []
+for player in players:
+    print('player:', player)
+    for age in ages:
+        print(player, age)
+        age_comb.append([player, age])
+
+# tuple
+age_comb2 = [(player, age) for player in players for age in ages]
+# list
+age_comb2 = [[player, age] for player in players for age in ages]
+print(age_comb2)
